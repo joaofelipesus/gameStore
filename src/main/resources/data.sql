634343a099a4a9362c273d9a4a9f72fa1affbb7f
joaofelipe <joaofelipe1294@gmail.com>
2021-06-04 21:05:28 -0300

    Added more console seeds.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-insert into console (name) values ('play 4');
+-- CONSOLES
+
+insert into console (name) values ('Play Station 4');
+insert into console (name) values ('XBox ONe');
+insert into console (name) values ('Nintendo Switch');
