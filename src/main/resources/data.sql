049e53e7cc722e9dfcc0a8ca8d5ed82b16ba8356
joaofelipe <joaofelipe1294@gmail.com>
2021-06-04 21:12:10 -0300

    Added category seeds.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,9 @@
 insert into console (name) values ('Play Station 4');
 insert into console (name) values ('XBox ONe');
 insert into console (name) values ('Nintendo Switch');
+
+-- CATEGORIES
+
+insert into category (name) values ('Shooter');
+insert into category (name) values ('Puzzle');
+insert into category (name) values ('Survival Horror');
