0abcbc170d2839ee4977798c3affcbf5d33b99dc
joaofelipe <joaofelipe1294@gmail.com>
2021-06-05 15:56:25 -0300

    added method show.

--- src/main/java/br/com/lopes/gameStore/controllers/CategoriesController.java
+++ src/main/java/br/com/lopes/gameStore/controllers/CategoriesController.java
@@ -2,12 +2,15 @@ package br.com.lopes.gameStore.controllers;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,22 +24,30 @@ import br.com.lopes.gameStore.repositories.CategoryRepository;
 @RestController
 @RequestMapping("/categories")
 public class CategoriesController {
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
-	
+
 	@GetMapping
-	public List<Category> index(){
+	public List<Category> index() {
 		return categoryRepository.findAll();
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Category> create(@RequestBody @Valid CategoryForm form, UriComponentsBuilder uriBuilder){
+	public ResponseEntity<Category> create(@RequestBody @Valid CategoryForm form, UriComponentsBuilder uriBuilder) {
 		Category category = form.toCategory();
 		categoryRepository.save(category);
 		URI uri = uriBuilder.path("/categories/{id}").buildAndExpand(category.getId()).toUri();
 		return ResponseEntity.created(uri).body(category);
 	}
-	
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Category> show(@PathVariable Long id) {
+		Optional<Category> categoryOptional = categoryRepository.findById(id);
+		if (categoryOptional.isPresent()) {
+			return ResponseEntity.ok(categoryOptional.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
 }
