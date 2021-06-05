13535dc5a6616a723bd05945b6735b35d4f55ddc
joaofelipe <joaofelipe1294@gmail.com>
2021-06-05 16:43:35 -0300

    Added game seeds.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,12 @@ insert into console (name) values ('Nintendo Switch');
 insert into category (name) values ('Shooter');
 insert into category (name) values ('Puzzle');
 insert into category (name) values ('Survival Horror');
+
+-- GAMES
+
+insert into game (title, price, category_id, console_id) values ('Resudent Evil 2 Remake', 299.90, 3, 1);
+insert into game (title, price, category_id, console_id) values ('God of war', 299.90, 2, 1);
+insert into game (title, price, category_id, console_id) values ('Alan wake', 299.90, 1, 1);
+insert into game (title, price, category_id, console_id) values ('Gears of war', 299.90, 3, 1);
+insert into game (title, price, category_id, console_id) values ('Zelda', 399.90, 2, 3);
+insert into game (title, price, category_id, console_id) values ('Mario World', 199.90, 2, 3);
