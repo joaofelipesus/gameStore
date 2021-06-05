63c4663b14177791bd8120cdfe34509133a26e93
joaofelipe <joaofelipe1294@gmail.com>
2021-06-05 17:00:42 -0300

    Added show route.

--- src/main/java/br/com/lopes/gameStore/controllers/GamesController.java
+++ src/main/java/br/com/lopes/gameStore/controllers/GamesController.java
@@ -1,12 +1,17 @@
 package br.com.lopes.gameStore.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.lopes.gameStore.controllers.exceptions.EntityNotFoundException;
+import br.com.lopes.gameStore.models.Category;
 import br.com.lopes.gameStore.models.Game;
 import br.com.lopes.gameStore.repositories.Gamerepository;
 
@@ -20,6 +25,21 @@ public class GamesController {
 	@GetMapping
 	public List<Game> index(){
 		return gameRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Game> show(@PathVariable Long id) {
+		Game game = findGame(id);
+		return ResponseEntity.ok(game);
+	}
+	
+	private Game findGame(Long id) {
+		Optional<Game> optional = gameRepository.findById(id);
+		if (optional.isPresent()) {
+			return optional.get();
+		} else {
+			throw new EntityNotFoundException();
+		}
 	}
 	
 	
