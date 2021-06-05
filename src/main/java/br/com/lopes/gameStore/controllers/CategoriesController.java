109074f577d857a27ceb089d6ec68112ce1648ba
joaofelipe <joaofelipe1294@gmail.com>
2021-06-05 16:29:28 -0300

    Added route delete.

--- src/main/java/br/com/lopes/gameStore/controllers/CategoriesController.java
+++ src/main/java/br/com/lopes/gameStore/controllers/CategoriesController.java
@@ -10,6 +10,7 @@ import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,6 +58,14 @@ public class CategoriesController {
 		Category category = findCategory(id);
 		category.setName(form.getName());
 		return ResponseEntity.ok(category);
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		findCategory(id);
+		categoryRepository.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 
 	private Category findCategory(Long id) {
